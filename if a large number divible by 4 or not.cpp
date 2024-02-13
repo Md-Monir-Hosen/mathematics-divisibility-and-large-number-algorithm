477959acdcd07d808032af7b3550bbc37e21e9b2
Md-Monir-Hosen <136980397+Md-Monir-Hosen@users.noreply.github.com>
2024-02-13 13:10:30 +0600

    Add files via upload

---
+++ if a large number divible by 4 or not.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    long long int n=1234567589333862;
+    if(n%4==0)
+    {
+        cout<<"Yes\n";
+    }
+    else
+    {
+        cout<<"NO \n";
+    }
+    return 0;
+}
